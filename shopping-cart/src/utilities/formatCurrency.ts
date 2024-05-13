421e37055b325e6c3be81d6339b358e86790b056
nara <parknara0213@gmail.com>
2024-05-14 00:04:59 +0900

    240514 Store page

---
+++ shopping-cart/src/utilities/formatCurrency.ts
@@ -0,0 +1,8 @@
+const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+  currency: "USD",
+  style: "currency",
+});
+
+export function formatcurrency(number: number) {
+  return CURRENCY_FORMATTER.format(number);
+}
